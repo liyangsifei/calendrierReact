37bae465dc53bfbe9824f6f82dcd57ed1e7461ca
sifei-dev <liyangsifei@gmail.com>
2020-05-14 01:17:00 +0200

    show coach name + fetch all pages + async button

--- src/App.js
+++ src/App.js
@@ -46,24 +46,62 @@ class Clock extends React.Component {
   }
 }
 
-const Sessions = ({ sessions }) => {
-  return (
-    <div>
-      <center><h2>Session List</h2></center>
-      {sessions.map((session) => (
-        <div class="card">
-          <div class="card-inside">
-            <div>Activity: {session.activity}</div>
-            <div>Coach: {session.coach}</div>
-            <div>Level: {session.level}</div>
-            <div>Date start: {session.date_start}</div>
-            <div>Duration: {session.duration_minute} min</div>
-          </div>
+function getTime(dateStr) {
+  return String(dateStr).slice(11, 19);
+}
+
+function getTimeZone(dateStr) {
+  return String(dateStr).slice(19);
+}
+
+function getDate(dateStr) {
+  return String(dateStr).slice(0, 10);
+}
+
+function getCoach(coachID) {
+  const coachAPI = 'https://back.staging.bsport.io/api/v1/coach/' + coachID;
+  let coachName;
+  fetch(coachAPI)
+    .then(res => res.json())
+    .then((data) => {
+      coachName = data.id;
+    })
+  return String(coachName);
+}
+
+
+class Sessions extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      // coachList: new Map(),
+      sessions: this.props.sessions,
+      fetchSth: "",
+    };
+  }
+
+  render() {
+      return (
+        <div>
+          <center><h2>Session List</h2></center>
+          <div>{this.state.fetchSth}</div>
+          {this.props.sessions.map((session) => (
+            <div class="card">
+              <div class="card-inside">
+                <div>Date: {getDate(session.date_start)}</div>
+                <div>Activity: {session.activity}</div>
+                <div>Coach: {this.props.coachs.get(session.coach)}</div>
+                <div>Level: {session.level}</div>
+                <div>Date start: {getTime(session.date_start)} ({getTimeZone(session.date_start)})</div>
+                <div>Duration: {session.duration_minute} min</div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  )
-};
+      )
+  }
+}
+
 
 class Day extends React.Component {
   constructor(props) {
@@ -126,74 +164,116 @@ class App extends Component {
         moment().add(6, 'days').format('YYYY-MM-DD'),
       ],
       date: moment().format('YYYY-MM-DD'),
+      all_sessions: [],
       sessions: [],
+      coachList: new Map(),
     };
+
+    this.doFetch();
   }
 
-  componentDidMount() {
-    const myAPI = 'https://back.staging.bsport.io/api/v1/offer/?company=6&page=4';
-    const today = this.state.date.toString();
-    fetch(myAPI)
-        .then(res => res.json())
-        .then((data) => {
-          let res = [];
-          let p = 0;
-          for (let k=0; k< data.results.length; k++) {
-            if (data.results[k].date_start.indexOf(today) > -1) {
-              res[p] = data.results[k];
-              p++;
-            }
-          }
-          this.setState({ sessions: res })
-        })
-        .catch(console.log);
+  doFetch() {
+    let date = this.state.date
+    let max_date = moment().add(6, 'days').format('YYYY-MM-DD')
+    let all_list = [];
+
+    const myAPI = 'https://back.staging.bsport.io/api/v1/offer/?company=6&min_date=' + date + '&max_date=' + max_date;
+
+    (async ()=>{
+      try {
+        await fetch(myAPI)
+         .then(res => res.json())
+         .then((data) => {
+           this.setState({
+             all_sessions: data.results
+           }, () => {
+             this.getCoachList();
+             this.updateList();
+           })
+         }).catch(console.log);
+
+      } catch(e) {
+        console.log(e);
+      }
+    })();
   }
-  componentWillUpdate() {
-    const myAPI = 'https://back.staging.bsport.io/api/v1/offer/?company=6&page=4';
-    const today = this.state.date.toString();
-    fetch(myAPI)
-        .then(res => res.json())
-        .then((data) => {
-          let res = [];
-          let p = 0;
-          for (let k=0; k< data.results.length; k++) {
-            if (data.results[k].date_start.indexOf(today) > -1) {
-              res[p] = data.results[k];
-              p++;
-            }
-          }
-          this.setState({ sessions: res })
+
+  async getCoachList() {
+    let id;
+    let name;
+    let myMap = this.state.coachList;
+    let sessions = this.state.all_sessions;
+
+    for (let i=0; i<sessions.length; i++) {
+      let session = this.state.all_sessions[i];
+      id = session.coach;
+      name = String(id);
+
+      let coachAPI = 'https://back.staging.bsport.io/api/v1/coach/' + id;
+      await fetch(coachAPI)
+        .then((response) => response.json())
+        .then(json => {
+          name = String(json.user.name);
+          myMap.set(id, name);
+
+        }).catch((error)=> {
+          console.log(error);
         })
-        .catch(console.log);
+
+      myMap.set(id, name);
+      this.setState({
+        coachList: myMap,
+      })
+    }
 
   }
-  handleSelect(i) {
+
+  updateList() {
+    const today = this.state.date.toString();
+    const list = this.state.all_sessions;
+    let today_list = [];
+    let p = 0;
+    for (let k=0; k < list.length; k++ ) {
+      if (list[k].date_start.indexOf(today) > -1) {
+        today_list[p] = list[k];
+        p++;
+      }
+    }
     this.setState({
-      date: this.state.weeks[i],
+      sessions: today_list
     })
-    this.render();
   }
+
+  async handleSelect(i) {
+    await this.setState({
+      date: this.state.weeks[i],
+    });
+    this.updateList();
+  }
+
   renderCalendar(i) {
     return (
+      <div>
       <Calendar
         weeks={i}
-        onClick={i => this.handleSelect(i)}
-      />
+        onClick={i => this.handleSelect(i)} />
+      </div>
     )
   }
   render() {
+    const sessions = this.state.sessions;
+    const coachs = this.state.coachList;
     return (
       <div className="App">
         <div className="App-header">
           <Title name={this.props.name}/>
           <Clock />
           {this.renderCalendar(this.state.weeks)}
-          <Sessions sessions={this.state.sessions} />
+          <Sessions sessions={sessions} coachs={coachs} />
         </div>
       </div>
     )
   };
 }
-
 
 export default App;
